51898ce4c7a0d8a5eb97ed839a703003803d9461
Elena Bezrodnova <lenyka_86@mail.ru>
2023-10-05 10:43:13 +0200

    added ArtistImplementation.java

---
+++ src/ArtistImplementation.java
@@ -0,0 +1,11 @@
+public class ArtistImplementation implements Artist {
+    @Override
+    public void setName(String name) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
