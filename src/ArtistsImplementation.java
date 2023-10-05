2e1c18c0b77b2e9a72da868a7bd9b6b4a92a628b
Elena Bezrodnova <lenyka_86@mail.ru>
2023-10-05 12:28:24 +0200

    implemented search method

--- src/ArtistsImplementation.java
+++ src/ArtistsImplementation.java
@@ -1,15 +1,18 @@
-public class ArtistsImplementation {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtistsImplementation implements Searchable {
 
     // private List<Artist> artists = new ArrayList<>();
 
     // private OwnList artists = new OwnList();
 
     // private Artist artist = new Artist();
-    private Artist[] artists = new Artist[4]; // remember the references
+    private Artist[] artists = new ArtistImplementation[4]; // remember the references
 
     private int count;
 
-    public void add(Artist artist) {
+    public void add(ArtistImplementation artist) {
         artists[count++] = artist;
     }
 
@@ -17,5 +20,19 @@ public class ArtistsImplementation {
         return artists[index];
     }
 
-    // search
+    @Override
+    public List<Artist> search(String search) {
+        List<Artist> searchResult = new ArrayList<>();
+            for (Artist artist: artists) {
+                boolean isMatched = artist.getName().toLowerCase().contains(search.toLowerCase());
+
+                if (isMatched) {
+                    searchResult.add(artist);
+                }
+            }
+        if (searchResult.size() > 0) {
+            System.out.println("Artist list is empty");
+        }
+        return searchResult;
+    }
 }
